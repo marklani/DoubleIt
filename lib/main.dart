a7211b2da71eba1baadd3952fe9efff07c075ef2
marklani <fatehlani@gmail.com>
2024-09-22 00:29:30 +0800

    commit again

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Double the Gifts?',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+        useMaterial3: true,
+      ),
+      home: SplashScreen()
+      //home: const MyHomePage(title: 'Double the Gifts?'),
+    );
+  }
+}
+
